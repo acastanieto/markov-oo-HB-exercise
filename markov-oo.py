7e35ec2dd3a662d049a6ca62e8087ea3840487cc
Hackbright Student <info@hackbrightacademy.com>
2015-04-16 19:59:06 -0700

    Added Mixin class to remove punctuation from string

--- markov-oo.py
+++ markov-oo.py
@@ -1,33 +1,79 @@
-import sys
+from sys import argv
 import random
-
-
-
-# Schematic:  
-# 1.    Move functions into SimpleMarkovGenerator class (now they're methods!)
-# 2.    Create a TweetMarkovGenerator subclass that has the same make_chains 
-#       method as the parent class
-# 3.    The TweetMarkovGenerator will have its own make_text method that inherits
-#       the make_text method from the Parent class but also tweaks it so that
-#       the Markov text generated ends at 140 characters.  
- 
 
 class SimpleMarkovGenerator(object):
     """Takes in text input, and generates a Markov string based on that 
     input using a Markov Chain algorithm."""
 
-    def make_chains(self, file_name): 
+    def read_text_files(self, argv):
+        string_list = []
+
+        for file_name in argv[1:]:
+            text_file = open(file_name)
+            text_string = text_file.read().replace("\n", " ").strip()
+            string_list.append(text_string)
+        return " ".join(string_list)
+
+    def make_chains(self, argv): 
         """Takes input text as string; returns dictionary of markov chains."""
 
-        text_file = open(file_name)
+        text_string = self.read_text_files(argv)
+        word_list = text_string.split()
 
-        text_string = text_file.read().replace("\n", " ").lower().strip()
+        bigrams = dict() 
+
+        for index, current_word in enumerate(word_list):
+            second_word = word_list[index + 1]
+            if index == len(word_list) - 2:
+                break
+            third_word = word_list[index + 2]
+
+            bigrams.setdefault((current_word, second_word), [])
+            bigrams[(current_word, second_word)].append(third_word)
+
+        return bigrams
+
+
+    def make_text(self, chains):
+        """Takes dictionary of markov chains; returns random text."""
+            
+        rand_key = random.choice(chains.keys())
+        key1, key2 = rand_key
+        markov_list = [key1, key2]       
+
+        while rand_key in chains:
+
+            rand_new_word = random.choice(chains[rand_key])
+            markov_list.append(rand_new_word)
+            rand_key = (rand_key[1], rand_new_word)
+           
+        return " ".join(markov_list)
+
+class RemovePunctuationMixin(object):
+    """Removes puncuation from your string"""
+
+    def remove_punct(self, text_string):
+
         text_no_punct = ""
 
         for char in text_string:
             if char.isalpha() or char == " ":
                 text_no_punct += char
 
+        return text_no_punct
+
+
+class SimpleMarkovGeneratorNoPunct(SimpleMarkovGenerator, RemovePunctuationMixin):
+    """Takes in text input, and generates a Markov string based on that 
+    input using a Markov Chain algorithm."""
+
+    def make_chains(self, argv): 
+        """Takes input text as string; returns dictionary of markov chains."""
+        
+        text_string = super(SimpleMarkovGeneratorNoPunct, self).read_text_files(argv)
+        text_no_punct = self.remove_punct(text_string)
+     
+        print text_no_punct
         word_list = text_no_punct.split()
 
         bigrams = dict() 
@@ -46,7 +92,7 @@ class SimpleMarkovGenerator(object):
     def make_text(self, chains):
         """Takes dictionary of markov chains; returns random text."""
             
-        rand_key = random.choice(chains.keys())
+        rand_key = random.choice(chains.keys())  
         key1, key2 = rand_key
         markov_list = [key1, key2]       
 
@@ -58,9 +104,16 @@ class SimpleMarkovGenerator(object):
            
         return " ".join(markov_list) + "."
 
+
+
+
+
 #  one way:  The TweetMarkovGenerator will have its own make_text method that inherits
 #  the make_text method from the Parent class but also tweaks it so that
 #  the Markov text generated ends at 140 characters.  
+
+
+
 class TweetableMarkovGeneratorDirty(SimpleMarkovGenerator):
     """Takes in text input, and generates a tweetable (140 characters or less) 
     Markov string based on that input using a Markov Chain algorithm."""
@@ -94,7 +147,6 @@ class TweetableMarkovGenerator(SimpleMarkovGenerator):
 
             for word in unaltered_words:
                 if len(shortened_string) + len(word) + 1 >= 140:
-                    shortened_string += "."
                     return shortened_string
 
                 else:
@@ -114,31 +166,6 @@ class TweetableMarkovGeneratorQuick(SimpleMarkovGenerator):
         """Takes dictionary of markov chains; returns random text that is
         140 characters long or less."""
             
-        # rand_key = random.choice(chains.keys())
-        # rand_value_word = random.choice(chains[rand_key])
-        # key1, key2 = rand_key
-        
-        # markov_string = key1 + " " + key2 + " " + rand_value_word
-        
-        # while rand_key in chains:
-
-
-        #     new_bigram = (key2, rand_value_word)
-
-        #     if new_bigram in chains:
-        #         rand_new_word = random.choice(chains[new_bigram])
-        #         if len(markov_string + rand_new_word) + 1 >= 140: # make inverse if cond
-        #             return markov_string + "."
-        #         else:
-        #             markov_string += " " + rand_new_word
-        #             key2 = rand_value_word
-        #             rand_value_word = rand_new_word 
-        #             rand_key = new_bigram           
-
-        #     else:
-        #         markov_string += "."
-        #         return markov_string
-
         rand_key = random.choice(chains.keys())
         key1, key2 = rand_key
         markov_list = [key1, key2]       
@@ -150,37 +177,31 @@ class TweetableMarkovGeneratorQuick(SimpleMarkovGenerator):
                 markov_list.append(rand_new_word)
                 rand_key = (rand_key[1], rand_new_word)
             else:   
-                return " ".join(markov_list) + ".", len(" ".join(markov_list) + ".")
+                return " ".join(markov_list)
 
-        return " ".join(markov_list) + ".", len(" ".join(markov_list) + ".")
+        return " ".join(markov_list)
             
-
-           
-        
-
     
-
-
-
-
-        
 if __name__ == "__main__":
-     
-    script, file_name = sys.argv
-
+      
     print "*" * 50
-    print "SimpleMarkovGenerator"
-    simple = SimpleMarkovGenerator()
-    print simple.make_text(simple.make_chains(file_name))
-    tweet = TweetableMarkovGenerator()
+    
+    # print "SimpleMarkovGenerator"
+    # simple = SimpleMarkovGenerator()
+    # print simple.make_text(simple.make_chains(argv))
+    # print "\n"
+
+    print "SimpleMarkovGeneratorNoPunct"
+    punct = SimpleMarkovGeneratorNoPunct()
+    print punct.make_text(punct.make_chains(argv))
     print "\n"
-    print "TweetableMarkovGenerator"
-    print tweet.make_text(tweet.make_chains(file_name))
-    print "\n"
-    qtweet = TweetableMarkovGeneratorQuick()
-    print "TweetableMarkovGeneratorQuick"
-    print qtweet.make_text(qtweet.make_chains(file_name))
-    print "\n"
-    dtweet = TweetableMarkovGeneratorDirty()
-    print "TweetableMarkovGeneratorDirty"
-    print dtweet.make_text(dtweet.make_chains(file_name))
+
+    # tweet = TweetableMarkovGenerator()
+    # print "\n"
+    # print "TweetableMarkovGenerator"
+    # print tweet.make_text(tweet.make_chains(argv))
+    
+    # print "\n"
+    # qtweet = TweetableMarkovGeneratorQuick()
+    # print "TweetableMarkovGeneratorQuick"
+    # print qtweet.make_text(qtweet.make_chains(argv))
